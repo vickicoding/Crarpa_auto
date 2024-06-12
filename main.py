8712a814bfc20e7e02a97f17a7b99617759ee90c
vickicoding <joaovitorpadua2004@gmail.com>
2024-06-12 00:36:00 -0300

    added name and directory functions, bug fixes

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ import time
 import platform
 
 
+name_config = "Config.json"
+
+
 # Função para criar o arquivo config caso não esteja criado
 def write_json(argument_config):
-    with open('config.json', 'w', encoding="utf-8") as arq:
+    with open(name_config, 'w', encoding="utf-8") as arq:
         json.dump(argument_config, arq, indent=4, ensure_ascii=False)
         
         
@@ -28,7 +31,7 @@ def clear_console():
 def escrever(word, next=True):
     for char in word:
         print(char, end="", flush=True)
-        time.sleep(0.005)
+        time.sleep(0.01)
     
     if next:
         print()
@@ -39,46 +42,79 @@ def escrever(word, next=True):
 def criar_pasta(diretorio, nome_cliente):
     caminho = os.path.join(diretorio, nome_cliente)
     os.makedirs(caminho, exist_ok=True)
-    escrever("Criação da pasta foi um sucesso!!!")
 
 
 # Função para criar um arquivo
 def criar_arquivo(diretorio, nome_cliente):
     caminho = os.path.join(diretorio, nome_cliente, nome_cliente + ".cdr")
-    with open(caminho, "w") as arquivo:
-        pass
-    escrever("Criação do arquivo foi um sucesso!!!")
+    if not os.path.exists(caminho):
+        with open(caminho, "w") as file:
+            pass
+        #Criação do arquivo foi um sucesso
+        return True
+    
+    else:
+        return False
 
+#Função para obter o diretório do usuário ou do arquivo caso exista
+def get_diretory():
+    if os.path.exists(name_config):
+        escrever("Carregando Configurações!!!")
+        diretorio = load_json(name_config)
+        clear_console()
+        
+    else:
+        escrever("Arquivo de configuração não encontrado...")
+        while True:
+            diretorio = input(escrever("Diretório: "))  # Exemplo: ~/Documents
+            
+            if not diretorio or not diretorio.strip():
+                clear_console()
+                escrever("Diretório não pode ser vazio!!!")
+            
+            else:
+                break        
+            
+            
+        dir = {
+        "diretory" : f"{diretorio}"
+        }
+        escrever("Salvando pré-configuração")
+        write_json(dir)
+        escrever("Arquivo config.json salvo!!!")
+        time.sleep(0.5)
+        clear_console()
+    
+    return diretorio
 
+#Função para receber um nome válido do usuário
+def get_name():
+    while True:
+        name = input(escrever("Nome do Cliente: "))
+        
+        if not name.strip():
+            clear_console()
+            escrever("Nome não pode ser vaio!!!")
+            time.sleep(0.5)
+        
+        else:
+            return name    
+            
 
 # Solicita o nome do cliente
-nome_cliente = input(escrever("Nome do Cliente: "))
+nome_cliente = get_name()
 clear_console()
 
+# Obter diretório
+diretorio = get_diretory()
 
-
-# Verifica se o arquivo de configuração existe
-if os.path.exists("config.json"):
-    escrever("Arquivo de pré-configuração encontrado!!!")
-    escrever("Analisando dados...")
-    diretorio = load_json("config.json")
-    clear_console()
-    
-else:
-    escrever("Arquivo de pré-configuração não encontrado.")
-    diretorio = input(escrever("Diretório: "))  # Exemplo: /storage/emulated/0/Documents
-    dir = {
-    "diretory" : f"{diretorio}"
-    }
-    escrever("Salvando pré-configuração")
-    write_json(dir)
-    escrever("Arquivo config.json salvo!!!")
-    time.sleep(0.5)
-    clear_console()
 
 print(f"Nome: {nome_cliente}\nCaminho: {diretorio}\n")
 
 # Criar pasta e arquivo
 criar_pasta(diretorio, nome_cliente)
-criar_arquivo(diretorio, nome_cliente)
+if criar_arquivo(diretorio, nome_cliente):
+    escrever("Sucess!!!")
 
+else:
+    escrever("Arquivo já existente")
